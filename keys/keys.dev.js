1284ada405f648a6a5fb3dff8c15944d41d7f7c6
Jurii <buga.jura@gmail.com>
2021-08-24 19:31:03 +0300

    3

---
+++ keys/keys.dev.js
@@ -0,0 +1,6 @@
+export default {
+    PASSWORD_MONGODB: 'a4IZhyeAVqLND6YO',
+    SECRET_KEY: 'buha',
+    API_KEY_SENDGRID: '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',
+    BASE_URL: "http://localhost:8080/"
+};
