63861f58ac171cd2c5e80fe36926b42cd25d2de4
Jurii <buga.jura@gmail.com>
2021-08-24 19:26:17 +0300

    2

--- keys/index.js
+++ keys/index.js
@@ -1,5 +1,5 @@
 import dev from './keys.dev.js';
 import prod from './keys.prod.js';
-const KEY = process.env.node_env === "production" ? prod : dev;
+const KEY = process.env.NODE_ENV === "production" ? prod : dev;
 
 export default KEY;
