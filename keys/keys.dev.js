e81ebae9408351123f9d684558754f863fdc749a
Fancules <36085506+Fancules@users.noreply.github.com>
2021-11-13 11:21:00 +0200

    Update keys.dev.js

--- keys/keys.dev.js
+++ keys/keys.dev.js
@@ -1,6 +1,6 @@
 export default {
-    PASSWORD_MONGODB: 'a4IZhyeAVqLND6YO',
+    PASSWORD_MONGODB: '',
     SECRET_KEY: 'buha',
-    API_KEY_SENDGRID: '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',
+    API_KEY_SENDGRID: '',
     BASE_URL: "http://localhost:8080/"
 };
